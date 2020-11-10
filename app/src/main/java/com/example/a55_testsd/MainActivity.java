cd665a27aa8a9f315278ebf014ee82c762dd6bb0
Sen-Bac0n <55573584+Sen-Bac0n@users.noreply.github.com>
2020-11-10 18:19:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a55_testsd/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.a55_testsd;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+    private void checkNeedPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //多个权限一起申请
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, 1);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkNeedPermissions();
+        Button btnWrite = findViewById(R.id.write);
+        Button btnRead = findViewById(R.id.read);
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OutputStream out=null;
+                try {
+                    if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        File file = Environment.getExternalStorageDirectory();
+                        File myfile = new File(file.getCanonicalPath() + "/"+"MyFileName");
+
+                        FileOutputStream fileOutputStream = new FileOutputStream(myfile);
+                        out = new BufferedOutputStream(fileOutputStream);
+                        String content = "hello world";
+                        try {
+                            out.write(content.getBytes(StandardCharsets.UTF_8));
+                        } finally {
+                            if (out != null)
+                                out.close();
+                        }
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream in=null;
+                try {
+                    if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                        File file = Environment.getExternalStorageDirectory();
+                        File myfile = new File(file.getCanonicalPath() + "/"+ "MyFileName");
+                        FileInputStream fileInputStream = new FileInputStream(myfile);
+                        in = new BufferedInputStream(fileInputStream);
+                        int c;
+                        StringBuilder stringBuilder = new StringBuilder("");
+                        try {
+                            while ((c = in.read()) != -1) {
+                                stringBuilder.append((char) c);
+                            }
+                            Toast.makeText(MainActivity.this, stringBuilder.toString(), Toast.LENGTH_LONG).show();
+                        } finally {
+                            if (in != null)
+                                in.close();
+                        }
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
+
+
